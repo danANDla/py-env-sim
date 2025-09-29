ce87892dcf64600f88338d48805ded130f96cdc1
mattykurlzz <mamontovvanya@yandex.ru>
2025-09-29 18:04:15 +0300

    add gravity

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import numpy as np
 
+GRAVITY_CONST = 9.81
+
 datatype = np.float32
 current_coordinates = np.array([0, 0], dtype=datatype)
 current_speed = np.array([0, 0], dtype=datatype)
 prev_speed = np.array([0, 0], dtype=datatype)
-current_acceleration = np.array([1, 1], dtype=datatype)
+current_force_applied = np.array([10000, 1], dtype=datatype)
+
+rocket_mass = 900
 
 time = 0
 time_step = 1
@@ -12,7 +16,11 @@ for _ in range(100):
     time += time_step
     prev_speed = current_speed
 
-    current_speed += current_acceleration * time_step 
+    real_acceleration = current_force_applied
+    real_acceleration[0] = current_force_applied[0] - GRAVITY_CONST * rocket_mass
+    if real_acceleration[0] < 0: real_acceleration[0] = 0
+
+    current_speed += current_force_applied * time_step 
     current_coordinates += ( current_speed * time_step + prev_speed * time_step ) / 2
 
     print(current_coordinates)
