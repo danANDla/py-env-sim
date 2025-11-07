e51e27f8e2444ae9c77bfc2e5dafc0d2c2408900
mattykurlzz <mamontovvanya@yandex.ru>
2025-11-07 14:10:25 +0300

    Feature: added rotational moment calculations

--- main.py
+++ main.py
@@ -12,11 +12,10 @@ R = TypeVar('R')
 P = ParamSpec('P')
 
 class Point:
-    x: float 
-    y: float
+    coords: np.ndarray
     
     def __init__(self, coords: tuple[float, float] = (0, 0)) -> None:
-        self.x, self.y = coords
+        self.coords = np.array([*coords])
 
 class Vector:
     dims: np.ndarray
@@ -32,7 +31,15 @@ class Vector:
         new_vec = Vector()
         
         new_vec.origins = from_vec.origins
-        new_vec.dims = to_vec.origins - from_vec.origins
+        new_vec.dims = to_vec.origins - from_vec.origins # todo: reuse from_two_points
+        
+        return new_vec
+    
+    def from_two_points(self, from_p: Point, to_p: Point) -> "Vector":
+        new_vec = Vector()
+
+        new_vec.origins = from_p.coords
+        new_vec.dims = to_p.coords - from_p.coords
         
         return new_vec
 
@@ -51,15 +58,20 @@ class Vector:
         self_k = self.dims[1] / self.dims[0] # todo: catch zero division
         self_b = self.origins[1] + ( -self.origins[0] * self_k )
         
-        other_k = 1/self_k
-        other_b = point.y - other_k * point.x
-        # todo: find intersection and len
+        other_k = -1/self_k
+        other_b = point.coords[1] - other_k * point.coords[0]
+        
+        x = ( other_b - self_b ) / ( self_k - other_k )
+        y = x * self_k + self_b
+        
+        len = Vector().from_two_points(Point(( x, y )), point).get_scalar_len()
+        return len
 
 class Transforms:
     """class provides basic vector transformations"""
     
     def rotate_ip(self, angle: float, transformant: Vector):
-        transform_matrix = np.array([[np.cos(angle), np.sin(angle)],
+        transform_matrix = np.array([[np.cos(angle), -np.sin(angle)],
                                      [np.sin(angle), np.cos(angle)]])
         transformant.dims = transformant.dims @ transform_matrix.T
     
@@ -119,7 +131,7 @@ class Engine:
 class RocketBody:
     dims: list[float]
     current_coordinates: np.ndarray 
-    main_vector: Vector
+    # main_vector: Vector
     
     def __init__(self, dims:tuple[float, float] = (5, 25), weight:float = 90, tilt: float = 0) -> None:
         self.dims = [dims[0], dims[1]]
@@ -130,7 +142,11 @@ class RocketBody:
         self.current_vector = transforms.rotate(tilt, Vector())
         
     def get_mass_centre(self): 
-        return self.current_coordinates
+        return self.current_vector.origins
+    
+    def add_tilt(self, angle): 
+        self.tilt += angle
+        transforms.rotate_ip(angle, self.current_vector)
 
 class Rocket: 
     forces: list[Force]
@@ -142,9 +158,12 @@ class Rocket:
     def __init__(self, forces: list[Force], engines: list[Engine], initial_tilt: float = 0):
         # delta movement of a rocket from prev step
         self.delta_coordinates = np.array([0, 0], dtype=datatype) 
-        self.current_speed: np.ndarray = np.array([0, 0], dtype=datatype)
-        self.prev_speed = np.array([0, 0], dtype=datatype)
-        self.current_force_applied = np.array([0, 0], dtype=datatype)
+        self.delta_angle = 0.
+        self.current_linear_speed: np.ndarray = np.array([0, 0], dtype=datatype)
+        self.current_rotation_speed: float = 0.
+        self.prev_linear_speed = np.array([0, 0], dtype=datatype)
+        self.prev_rotation_speed: float = 0.
+        self.zero_vector = np.array([0, 0], dtype=datatype)
         
         self.body_model = RocketBody(tilt = initial_tilt)
 
@@ -155,33 +174,45 @@ class Rocket:
     def time_step(self, time_step: float):
         self.prev_time += time_step
 
-        self.prev_speed: np.ndarray = self.current_speed.copy()
-        real_acceleration = self.current_force_applied.copy()
+        self.prev_linear_speed: np.ndarray = self.current_linear_speed.copy()
+        self.prev_rotation_speed: float = self.current_rotation_speed
+
+        real_linear_acceleration = self.zero_vector.copy()
+        real_rotation_acceleration: float = 0.
 
         for force in self.forces:
             arg_list: list[float] = []
             for parameter_name in force.attribute_names:
                 arg_list.append(operator.attrgetter(parameter_name)(self))
-            real_acceleration = force.update(real_acceleration, *arg_list) # type: ignore
+            real_linear_acceleration = force.update(real_linear_acceleration, *arg_list) # type: ignore
         
         for engine in self.engines:
             engine_force_vector = engine.renew_force_vector(self.body_model.current_vector)
             engine_power = engine.current_force
+            force_vector = engine_force_vector.dims * -1. * engine_power
+            force_scalar = float(np.sqrt(force_vector[0] ** 2 + force_vector[1] ** 2))
+
+            real_linear_acceleration += force_vector / self.body_model.weight
             
+            rotation_moment = force_scalar * engine_force_vector.distance_to(Point((self.body_model.get_mass_centre()[0], self.body_model.get_mass_centre()[1])))
+            inertia_moment = self.body_model.dims[0] * self.body_model.dims[1] * (self.body_model.dims[0] ** 2 + self.body_model.dims[1] ** 2) / 12
+            
+            real_rotation_acceleration += rotation_moment / inertia_moment
 
-            real_acceleration += engine_force_vector.dims * -1. * engine_power
+        self.current_linear_speed += real_linear_acceleration * time_step 
+        self.current_rotation_speed += real_rotation_acceleration * time_step
 
-        self.current_speed += real_acceleration * time_step 
-
-        self.delta_coordinates = ( self.current_speed * time_step + self.prev_speed * time_step ) / 2
+        self.delta_coordinates = ( self.current_linear_speed * time_step + self.prev_linear_speed * time_step ) / 2
+        self.delta_angle = ( self.current_rotation_speed * time_step + self.prev_rotation_speed * time_step ) / 2
         self.body_model.current_coordinates += self.delta_coordinates
+        self.body_model.add_tilt(self.delta_angle)
         
 def init_rocket() -> Rocket:
     main_engine_transforms: Callable[[Vector], Vector] = lambda v: transforms.rotate(np.pi, transforms.move_along(np.array([0, -10]), v))
-    main_engine = Engine(2000, 580, main_engine_transforms)
+    main_engine = Engine(200000, 58000, main_engine_transforms)
 
-    rotating_engine_transforms: Callable[[Vector], Vector] = lambda v: transforms.rotate(np.pi / 2, v)
-    rotating_engine = Engine(100, 100, rotating_engine_transforms)
+    rotating_engine_transforms: Callable[[Vector], Vector] = lambda v: transforms.rotate(np.pi / 2, transforms.move_along(np.array([0, -10]), v))
+    rotating_engine = Engine(1000, 1000, rotating_engine_transforms)
 
     def gravity_func(force: np.ndarray, mass: float) -> np.ndarray:
         force[1] -= GRAVITY_CONST * mass
@@ -216,7 +247,7 @@ def launch_sim(screen: pygame.Surface, center_offset: tuple[int, int]):
             if event.type == pygame.QUIT:
                 running = False
 
-        dt = clock.tick(fps) / 1000
+        dt = clock.tick(fps) / 1000 # todo
         myrocket.time_step(dt)
         
         screen.fill("white")
@@ -224,7 +255,7 @@ def launch_sim(screen: pygame.Surface, center_offset: tuple[int, int]):
         old_center = rect.center
 
         angle = myrocket.body_model.tilt * 180 / np.pi
-        rotated_surface = pygame.transform.rotate(rocket_surface, angle=angle % 360)
+        rotated_surface = pygame.transform.rotate(rocket_surface, angle=angle * -1 % 360)
         rotated_rect = rotated_surface.get_rect()
         rotated_rect.center = old_center
 
@@ -232,7 +263,7 @@ def launch_sim(screen: pygame.Surface, center_offset: tuple[int, int]):
         pygame.display.flip()
         
         for engine in myrocket.engines:
-            engine.set_force(engine.current_force + 10)
+            engine.set_force(engine.current_force + 500)
 
         print(myrocket.body_model.current_coordinates)
 
